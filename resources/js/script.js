95b02d56b9f9ac998c9c577cd6cbc29bae400b1e
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2021-12-09 14:14:44 +0630

    add sticky nav bar

---
+++ resources/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.js--section-feature').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '150px'
+    })
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   })
+});
